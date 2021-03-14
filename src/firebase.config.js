5a019413edcb91b7cdd54c61f807b06bceda89eb
Rifat91 <mhrifat11999@gmail.com>
2021-03-15 02:21:40 +0600

    authentication doen

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBj-dHoCVaARhIuvE5rAf9RuOs6eFil3uM",
+    authDomain: "ema-john-simple-9337a.firebaseapp.com",
+    projectId: "ema-john-simple-9337a",
+    storageBucket: "ema-john-simple-9337a.appspot.com",
+    messagingSenderId: "71921172686",
+    appId: "1:71921172686:web:6f195966e7b6b0b643d09f"
+  };
+
+  export default firebaseConfig;
